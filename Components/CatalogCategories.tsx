2183777dfd50137cd32ea30d6a2feeaed3ee794f
Vladimir Kornienko <VladKorn@VladKorn.local>
2019-10-22 19:12:05 +0300

    end catalog optimization

--- Components/CatalogCategories.tsx
+++ Components/CatalogCategories.tsx
@@ -1,43 +1,11 @@
-import React, { Component } from 'react'
-import {
-	View,
-    Text,
-    StyleSheet,
-    SectionList
-} from "react-native";
+import React, { Component } from "react";
+import { View, Text, StyleSheet, SectionList } from "react-native";
 import ProductItem from "./ProductItem";
 import appStyles from "./appStyles";
-import Loading from './Loading'
-// const DATA = [
-//     {
-//       title: 'Main dishes',
-//       data: ['Pizza', 'Burger', 'Risotto'],
-//     },
-//     {
-//       title: 'Sides',
-//       data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
-//     },
-//     {
-//       title: 'Drinks',
-//       data: ['Water', 'Coke', 'Beer'],
-//     },
-//     {
-//       title: 'Desserts',
-//       data: ['Cheese Cake', 'Ice Cream'],
-//     },
-//   ];
-let data = [
+import Loading from "./Loading";
 
-]
-function Item({ title }) {
-return (
-    <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
-    </View>
-);
-}
-export default class CatalogCategories extends Component<any , any> {
-    constructor(props) {
+export default class CatalogCategories extends Component<any, any> {
+	constructor(props) {
 		super(props);
 		this.state = {
 			isLoading: true,
@@ -47,55 +15,54 @@ export default class CatalogCategories extends Component<any , any> {
 			// searchRes: [1949 ,1946 ,1355],
 		};
 		// this.search = this.search.bind(this);
-    }
-    shouldComponentUpdate(nextProps, nextState) {
-		// console.log(nextProps.screenProps.favorite.length , this.props.screenProps.favorite.length);
+	}
+	shouldComponentUpdate(nextProps, nextState) {
+		// return true;
+        // console.log(nextProps.screenProps.favorite.length , this.props.screenProps.favorite.length);
 		// if(nextProps.screenProps.favorite.length !== this.props.screenProps.favorite.length){
 		//     return true
 		// } else{
 		//     return this.state.isLoading
-        // }
-        // return true;
+		// }
+		// return true;
 		// if (Object.keys(nextProps.screenProps.basket).length !== Object.keys(this.props.screenProps.basket).length) {
-		// 	return true;        
-        // }        
+		// 	return true;
+		// }
 		if (nextProps.searchRes.length !== this.props.searchRes.length) {
 			return true;
-        }  
-        if (this.props.navigation.state.params) {
+        }
+		if (this.props.navigation.state.params) {
+            console.log('this.props.navigation.state.params', this.props.navigation.state.params);
 			if (
 				nextProps.navigation.state.params.innerCatId !==
 					this.props.navigation.state.params.innerCatId ||
 				nextProps.navigation.state.params.catId !==
 					this.props.navigation.state.params.catId
 			) {
+                return true;
 			}
-			return true;
 		} else {
-			return this.state.isLoading;
+            if (nextProps.navigation.state.params && !this.props.navigation.state.params) {
+                return true;
+            } 
+            return this.state.isLoading;
 		}
-    }
-                   
-    componentDidMount(){
-        setTimeout(() => {
-            this.setState({isLoading: false});
-        }, 200);
-    }
+	}
 
-    render() {
-        // if(this.state.isLoading){return null}
-
-        // return(<SectionList
-        //     sections={DATA}
-        //     keyExtractor={(item, index) => item + index}
-        //     renderItem={({ item }) => <Item title={item} />}
-        //     renderSectionHeader={({ section: { title } }) => (
-        //       <Text style={styles.header}>{title}</Text>
-        //     )}
-        //   />);
-        if(this.state.isLoading){
-            return (<View style={{flex:1 , justifyContent: 'center'}}><Loading></Loading></View>);
-        }
+	componentDidMount() {
+		setTimeout(() => {
+			this.setState({ isLoading: false });
+		}, 30);
+	}
+    
+	render() {
+		if (this.state.isLoading) {
+			return (
+				<View style={{ flex: 1, justifyContent: "center" }}>
+					<Loading></Loading>
+				</View>
+			);
+		}
 		const catId = this.props.navigation.state.params
 			? this.props.navigation.state.params.catId
 			: false;
@@ -109,26 +76,22 @@ export default class CatalogCategories extends Component<any , any> {
 			catalog[catId] = this.props.catalog[catId];
 		} else {
 			catalog = this.props.catalog;
-		}
-		// let products = []
-		// if(){
-
-		// }else{
-
-		// }
-		// console.log('params' , this.props.navigation.state.params);
+        }
 		const isFavorites = this.props.navigation.state.params
 			? this.props.navigation.state.params.isFavorite || false
 			: false;
 
-		const products = this.props.products;
+        const products = this.props.products;
+        let data = [];
 		// console.log('products' , products);
-		let cats = catalog
+		catalog
 			? Object.keys(catalog).map(key => {
 					let cat = catalog[key];
-					let items = cat.products
+					let items = [];
+					cat.products
 						? cat.products.map(pkey => {
 								let item = products[pkey];
+								let isIncludet = true;
 								// console.log('item' , this.state.searchRes ,pkey);
 
 								// is in search
@@ -138,7 +101,7 @@ export default class CatalogCategories extends Component<any , any> {
 											parseInt(pkey)
 										)
 									) {
-										return false;
+										isIncludet = false;
 									}
 								}
 								// is in favorites
@@ -148,7 +111,7 @@ export default class CatalogCategories extends Component<any , any> {
 										parseInt(item.id)
 									)
 								) {
-									return false;
+									isIncludet = false;
 								}
 
 								// is in inner category
@@ -162,7 +125,7 @@ export default class CatalogCategories extends Component<any , any> {
 											parseInt(item.id)
 										)
 									) {
-										return false;
+										isIncludet = false;
 									}
 								}
 								//
@@ -170,68 +133,45 @@ export default class CatalogCategories extends Component<any , any> {
 									parseInt(item.id)
 								);
 								// console.log('render' , item.id);
-								return (
-									<ProductItem
-										key={item.id}
-										id={item.id}
-										name={item.name}
-										img={item.img}
-										price={item.price}
-										basketApi={
-											this.props.basketApi
-										}
-										addToFavorite={
-											this.props.addToFavorite
-										}
-										navigation={this.props.navigation}
-										isFavorite={isFavorite}
-										searchWords={this.props.search.split(' ')}
-									/>
-								);
+								isIncludet
+									? items.push({
+											key: item.id,
+											id: item.id,
+											name: item.name,
+											img: item.img,
+											price: item.price,
+											basketApi: this.props.basketApi,
+											addToFavorite: this.props
+												.addToFavorite,
+											navigation: this.props.navigation,
+											isFavorite: isFavorite,
+											searchWords: this.props.search.split(
+												" "
+											)
+									  })
+									: null;
 						  })
-                        : [];
-                    data.push({
-                        title: cat.name,
-                    });
-					return (
-						<View key={cat.id}>
-							<Text
-								style={[
-									appStyles.sectTitle,
-									{ marginLeft: 25 }
-								]}
-							>
-								{cat.name}
-							</Text>
-							{items}
-						</View>
-					);
+						: [];
+					data.push({
+						title: cat.name,
+						data: items
+					});
 			  })
 			: [];
-        return (
-            <View>
-                {cats}
-               
-            </View>
-        )
-    }
+		return (
+			<View>
+				{/* {cats} */}
+				<SectionList
+                    sections={data}
+					keyExtractor={(item, index) => item.id + index}
+					renderItem={({ item }) => <ProductItem {...item} />}
+					renderSectionHeader={({ section: { title } }) => (
+						<Text style={[appStyles.sectTitle, { marginLeft: 25 }]}>
+							{title}
+						</Text>
+					)}
+				/>
+			</View>
+		);
+	}
 }
-
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      marginHorizontal: 16,
-    },
-    item: {
-      backgroundColor: '#f9c2ff',
-      padding: 20,
-      marginVertical: 8,
-    },
-    header: {
-      fontSize: 32,
-    },
-    title: {
-      fontSize: 24,
-    },
-  });
