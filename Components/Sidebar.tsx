a1e3135c726a8395dbdaaec8d96842a7986527bc
VladKorn <webvladkorn@gmail.com>
2020-02-14 19:31:08 +0200

    ...

--- Components/Sidebar.tsx
+++ Components/Sidebar.tsx
@@ -179,7 +179,7 @@ export default class Sidebar extends React.Component<any, State> {
 
                     {menu}
                 </View>
-                <Text style={{fontSize: 12 , marginTop: 'auto' , textAlign: 'center'}}>version: 0.9.8</Text>
+                <Text style={{fontSize: 12 , marginTop: 'auto' , textAlign: 'center'}}>version: 1.0.1</Text>
             </SafeAreaView>
         );
     }
